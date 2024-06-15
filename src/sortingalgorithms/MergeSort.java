df0fbff2ae8f82545478792643b6e3af1f95e297
AnzhelDimitrov <108436278+AnzhelDimitrov@users.noreply.github.com>
2024-06-16 01:52:26 +0300

    Added a working implementation of the Merge Sort. Next step will be to implement multithreading for it

---
+++ src/sortingalgorithms/MergeSort.java
@@ -0,0 +1,37 @@
+package sortingalgorithms;
+
+import java.util.Arrays;
+
+public class MergeSort implements SortingAlgorithm {
+    public void sort(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = Arrays.copyOfRange(arr, 0, mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+        sort(left);
+        sort(right);
+
+        merge(arr, left, right);
+    }
+    private void merge(int[] result, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+        // Copy remaining elements (if any)
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+    }
+}
